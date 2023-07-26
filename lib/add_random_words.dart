d0dca607c96fccc643b00e374a8b7d340c09aa48
Shadman Saif <shadmansaif@outlook.com>
2023-07-26 10:23:07 +0600

    Ready to be used as a boiler plate

--- lib/add_random_words.dart
+++ lib/add_random_words.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
 void addWordPair(BuildContext context, Function(WordPair) updateWordPairs) {
-  TextEditingController textEditingController = TextEditingController();
+  TextEditingController firstWordController = TextEditingController();
+  TextEditingController secondWordController = TextEditingController();
 
   void handleFormSubmission() {
-    String inputText = textEditingController.text;
-    WordPair newWordPair = WordPair(inputText, '.');
+    String firstWord = firstWordController.text;
+    String secondWord = secondWordController.text;
+
+    WordPair newWordPair = WordPair(firstWord, secondWord);
 
     // Update the original _randomWordPairs list using the provided callback function.
-    updateWordPairs(
-      newWordPair,
-    );
+    updateWordPairs(newWordPair);
 
     Navigator.of(context).pop(); // Pop the current route to go back.
   }
@@ -26,12 +27,24 @@ void addWordPair(BuildContext context, Function(WordPair) updateWordPairs) {
         child: Column(
           children: [
             TextField(
-              controller: textEditingController,
+              controller: firstWordController,
+              decoration: const InputDecoration(
+                  labelText: 'First word', contentPadding: EdgeInsets.all(8.0)),
+            ),
+            TextField(
+              controller: secondWordController,
+              decoration: const InputDecoration(
+                  labelText: 'Second word',
+                  contentPadding: EdgeInsets.all(8.0)),
               onSubmitted: (_) => handleFormSubmission(),
             ),
-            ElevatedButton(
-              onPressed: handleFormSubmission,
-              child: const Text('Add WordPair'),
+            Container(
+              margin: const EdgeInsets.only(
+                  top: 16), // Set the top margin value as desired
+              child: ElevatedButton(
+                onPressed: handleFormSubmission,
+                child: const Text('Add WordPair'),
+              ),
             ),
           ],
         ),
